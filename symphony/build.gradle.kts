e0bdd3755a28e50a9593dcc9549ee2dc08ee8f9f
andylamax <andylamax@programmer.net>
2023-04-30 03:37:15 +0300

    Gradle configuration has stopped freaking out

--- symphony/build.gradle.kts
+++ symphony/build.gradle.kts
@@ -18,7 +18,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                api(projects.symphonyInputsCore)
+                // api(projects.symphonyInputsCore)
                 api(projects.kashMoney)
             }
         }
